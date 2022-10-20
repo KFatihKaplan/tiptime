85d533d85ac2825c6b0ab3ea0b5d97aac1ad92dc
Kemalettin Fatih Kaplan <kfatihkaplan@gmail.com>
2022-10-20 23:44:32 +0300

    yorum satırı yazıldı

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
+            //deneme
         }
         var tip = tipPercentage * cost
         val roundUp = binding.roundUpSwitch.isChecked
